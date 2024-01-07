4ca688e4e3e79abbb299daff50a35fba7cc5797e
Ayush Gupta <guptaayush212004@gmail.com>
2024-01-08 00:40:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header'
+import Meme from './components/meme'
+// import Meme from './components/testmeme'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
